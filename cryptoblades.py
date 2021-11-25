490b6ef8e094354996ac1e64fa5bde76c65d3c92
Heki <h3k1ru@yandex.ru>
2021-11-25 03:40:37 +0300

    initial mongodb support

--- cryptoblades.py
+++ cryptoblades.py
@@ -30,27 +30,27 @@ class Cryptoblades:
         self.w3.middleware_onion.inject(geth_poa_middleware, layer=0)
         # cryptoblades
         self.cryptoblades_address = self.w3.toChecksumAddress(self.config['cryptoblades_address'])
-        with open(config['cryptoblades_abi']) as f:
+        with open(config['abi']['cryptoblades']) as f:
             self.cryptoblades_abi = json.loads(f.read())
         self.cryptoblades_contract = self.w3.eth.contract(address=self.cryptoblades_address, abi=self.cryptoblades_abi)
         # weapons
         self.weapons_address = self.w3.toChecksumAddress(self.config['weapons_address'])
-        with open(config['weapons_abi']) as f:
+        with open(config['abi']['weapons']) as f:
             self.weapons_abi = json.loads(f.read())
         self.weapons_contract = self.w3.eth.contract(address=self.weapons_address, abi=self.weapons_abi)
         # characters
         self.characters_address = self.w3.toChecksumAddress(self.config['characters_address'])
-        with open(config['characters_abi']) as f:
+        with open(config['abi']['characters']) as f:
             self.characters_abi = json.loads(f.read())
         self.characters_contract = self.w3.eth.contract(address=self.characters_address, abi=self.characters_abi)
         # raid
         self.raid_address = self.w3.toChecksumAddress(self.config['raid_address'])
-        with open(config['raid_abi']) as f:
+        with open(config['abi']['raid']) as f:
             self.raid_abi = json.loads(f.read())
         self.raid_contract = self.w3.eth.contract(address=self.raid_address, abi=self.raid_abi)
         # oracle
         self.oracle_address = self.w3.toChecksumAddress(self.config['oracle_address'])
-        with open(config['oracle_abi']) as f:
+        with open(config['abi']['oracle']) as f:
             self.oracle_abi = json.loads(f.read())
         self.oracle_contract = self.w3.eth.contract(address=self.oracle_address, abi=self.oracle_abi)
 
---
+++ db.py
@@ -0,0 +1,11 @@
+import yaml
+from pymongo import MongoClient
+
+
+class DB:
+    def __init__(self):
+        with open('./config.yaml') as f:
+            config = yaml.full_load(f)
+        self.config = config['mongodb']
+        self.client = MongoClient(f'mongodb://{self.config["login"]}:{self.config["password"]}@'
+                                  f'{self.config["host"]}:{self.config["port"]}/?authSource={self.config["source"]}')
